8926a8aaf8875d64c6dd8a80ec70f14d7fe9f8c6
Dejnov <daniel.cerny@tul.cz>
2024-12-05 12:14:40 +0100

    015myapp

---
+++ MyApp015aSharedTaskList/app/src/main/java/com/example/myapp015asharedtasklist/Task.kt
@@ -0,0 +1,11 @@
+package com.example.myapp015asharedtasklist
+
+data class Task(
+    val id: String, // Identifikátor úkolu (pro Firestore)
+    var name: String, // Název úkolu
+    var isCompleted: Boolean = false, // Stav dokončení
+    var assignedTo: String = "" // Kdo si úkol vzal
+) {
+    // No-argument constructor required for Firestore deserialization
+    constructor() : this(null.toString(), null.toString())
+}
